3c1cff1379278e65e604439ac7ac74f896980a1e
margo <margo171190@mail.ru>
2023-06-04 15:24:25 +0400

    Homework 2.3 is done!

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,10 +1,16 @@
-public class Bicycle {
-
-    public String modelName;
-    public int wheelsCount;
-
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+public class Bicycle extends Vehicle implements Check {
+    //    public Bicycle(String modelName, int wheelsCount) {
+//        super(modelName, wheelsCount);
+//    }
+    @Override
+    public void check() {
+        if (this != null) {
+            System.out.println("Обслуживаем " + this.getModelName());
+            for (int i = 0; i < this.getWheelsCount(); i++) {
+                this.updateTyre();
+            }
+        }
     }
+
 }
 
--- src/Car.java
+++ src/Car.java
@@ -1,12 +1,16 @@
-public class Car {
-    public String modelName;
-    public int wheelsCount;
+public class Car extends Vehicle implements Check {
 
-    public void updateTyre() {
-        System.out.println("Меняем покрышку");
+    public void check() {
+        if (this != null) {
+            System.out.println("Обслуживаем " + this.getModelName());
+            for (int i = 0; i < this.getWheelsCount(); i++) {
+                this.updateTyre();
+            }
+            this.checkEngine();
+        }
     }
 
-    public void checkEngine() {
+        public void checkEngine() {
         System.out.println("Проверяем двигатель");
     }
 }
